2b65f68ea82cf20f8ebf1178e209598154f90159
Savin Angel-Mario <savin@angelmario.eu>
2023-07-21 03:19:35 +0300

    Updated door_knocking

--- scripts/scripted/door_knock.gd
+++ scripts/scripted/door_knock.gd
@@ -1,11 +1,21 @@
 extends Door
 
-@onready var audio_stream: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var audio_stream: AudioStreamPlayer3D = $KnockingStream
+@onready var stinger_stream: AudioStreamPlayer = $StingerStream
 @onready var timer = $Timer
+
+var stinger_played = false
+
+func _ready():
+	timer.start(2)
 
 func interact():
 	super.interact()
 	audio_stream.stop()
+	
+	if !stinger_played:
+		stinger_stream.play()
+		stinger_played = true
 	
 	if !self.open:
 		timer.start(5)
